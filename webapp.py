36da038db046f9e2d1ef6dc6b7a525356b3e072c
kylan02 <31520026+kylan02@users.noreply.github.com>
2019-10-11 09:17:59 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/response")
+def render_response():
+    favorite_color = request.args['color'] #get user's input for color input
+    if favorite_color == "pink":
+        response = "That's my favorite color too!"
+    else: 
+        response = "My favorite color is Pink."
+    return render_template('response.html', responseFromServer=response)
+    
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
