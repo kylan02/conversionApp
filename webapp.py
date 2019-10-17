fdb077e190a5448b03bbe92ce64de09d5a3c9cd0
Kylan O'Connor <kylan02@gmail.com>
2019-10-17 08:58:50 -0700

    commit

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
+
+x = 0
 
 @app.route("/")
 def render_main():
@@ -8,19 +10,24 @@ def render_main():
 
 @app.route("/dollarToEuro")
 def render_page1():
-    return render_template('dollar-euro.html')
+	x = 0.91
+	return render_template('dollar-euro.html')
 
 @app.route("/FeetToMeters")
 def render_page2():
-    return render_template('feet-meters.html')
+	x = 0.3048
+	return render_template('feet-meters.html')
     
 @app.route("/PoundToKg")
 def render_page3():
-    return render_template('pound-kg.html')
+	x = 0.453592
+	return render_template('pound-kg.html')
     
 @app.route("/response")
-def render_page3():
-    return render_template('response.html')
+def render_page4():
+	currency_to_convert = request.args['amount']
+	response = str(float(currency_to_convert)*(x)) + " Euros"
+	return render_template('response.html', responseFromServer=response)
     
 if __name__=="__main__":
     app.run(debug=False)
