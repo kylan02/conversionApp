430f3d1c7143cb90fc5b1c79760eb5834a15227b
Kylan O'Connor <kylan02@gmail.com>
2019-10-17 09:51:02 -0700

    commit

--- webapp.py
+++ webapp.py
@@ -2,26 +2,32 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-x = 0
-
-@app.route("/")
-def render_main():
-    return render_template('home.html')
-
 @app.route("/dollarToEuro")
 def render_page1():
-	x = 0.91
-	return render_template('dollar-euro.html')
+	if "amount" in request.args:
+		currency_to_convert = request.args['amount']
+		response = str((round(100*(float(currency_to_convert)*(0.91)))/100)) + " Euros"
+		return render_template('dollar-euro.html', responseFromServer=response)
+	else:
+		return render_template('dollar-euro.html')
 
 @app.route("/FeetToMeters")
 def render_page2():
-	x = 0.3048
-	return render_template('feet-meters.html')
+	if "amount" in request.args:
+		currency_to_convert = request.args['amount']
+		response = str((round(100*(float(currency_to_convert)*(0.3048)))/100)) + " Meters"
+		return render_template('feet-meters.html', responseFromServer=response)
+	else:
+		return render_template('feet-meters.html')
     
 @app.route("/PoundToKg")
 def render_page3():
-	x = 0.453592
-	return render_template('pound-kg.html')
+	if "amount" in request.args:
+		currency_to_convert = request.args['amount']
+		response = str((round(100*(float(currency_to_convert)*(0.453592)))/100)) + " Kg"
+		return render_template('pound-kg.html', responseFromServer=response)
+	else:
+		return render_template('pound-kg.html')
     
 @app.route("/response")
 def render_page4():
